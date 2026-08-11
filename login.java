dcb8730270265f2fc47190040232273cb7436fa5
kanchana <archanagd78@gmail.com>
2026-08-11 22:51:37 +0530

    added dev files

--- login.java
+++ login.java
@@ -1,5 +1,9 @@
 class login{
     public static void main(String[] args){
+<<<<<<< HEAD
         System.out.println("login updated in main");
+=======
+        System.out.println("login is updated");
+>>>>>>> feature
     }
 }
